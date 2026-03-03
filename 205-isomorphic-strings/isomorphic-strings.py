5a09c6b9eb796e36d892289bddd099fb724ab288
Prajjwal Sen <prajjwalsen7@gmail.com>
2026-03-03 22:30:27 +0530

    Time: 3 ms (91.61%) | Memory: 19.5 MB (11.27%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+            return False 
+        mapping = {}
+        for i,j in zip(s,t):
+            mapping[i]=j
+        if(len(set(mapping.values())) != len(mapping.values())):
+            return False
+        newstr=''
+        for i in s:
+            newstr += mapping[i]
+        if(newstr == t):
+            return True
+        else:
+            return False
+        
