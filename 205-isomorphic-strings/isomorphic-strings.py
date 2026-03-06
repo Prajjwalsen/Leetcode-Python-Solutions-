feb23bc50cb758a4e997d87d6ceeb7b451d6912c
Prajjwal Sen <prajjwalsen7@gmail.com>
2026-03-06 21:44:13 +0530

    Time: 3 ms (91.45%) | Memory: 19.4 MB (24.80%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,16 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if(len(s) != len(t)):
+        if(len(s)!=len(t)):
             return False 
         mapping = {}
         for i,j in zip(s,t):
-            mapping[i]=j
+            mapping[i] = j
         if(len(set(mapping.values())) != len(mapping.values())):
             return False
-        newstr=''
+        newstr = ''
         for i in s:
             newstr += mapping[i]
-        if(newstr == t):
+        if (newstr == t):
             return True
         else:
             return False
