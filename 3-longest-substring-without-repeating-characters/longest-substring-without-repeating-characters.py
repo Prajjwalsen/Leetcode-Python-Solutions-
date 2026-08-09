55bda1e68935330189630937568833300ed7f57e
Prajjwal Sen <prajjwalsen7@gmail.com>
2026-08-09 14:31:06 +0530

    Time: 206 ms (18.32%) | Memory: 20.1 MB (7.68%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,14 +5,15 @@ class Solution:
         max_length = 0
 
         for right in range(len(s)):
-
-            while s[right] in char_set:
+            while(s[right]) in char_set:
                 char_set.remove(s[left])
                 left += 1
-            
             char_set.add(s[right])
 
             max_length = max(max_length, len(char_set))
 
         return max_length
+
+
+
         
